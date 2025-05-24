3c88a101f45b27c571a9a9350fcc5c379de5c19e
Nulily <qcjsorticio@tip.edu.ph>
2025-05-24 16:10:47 +0800

    Create streamlit_app.py
    Deploy model using streamlit app

---
+++ streamlit_app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import numpy as np
+import os
+import urllib.request
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from PIL import Image
+
